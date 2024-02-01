bce6d9d0b188afc336c0c628231563cf25ad201f
toothlessdev <toothless042@gmail.com>
2024-02-01 23:06:48 +0900

    A+B

---
+++ 2024-02/1000.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const input = fs
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "../input.txt")
+    .toString()
+    .trim()
+    .split(" ")
+    .map(Number);
+
+console.log(input[0] + input[1]);
