defb9321f12d36dbb60479cbb042424140b3728e
toothlessdev <toothless042@gmail.com>
2024-04-16 20:03:11 +0900

    build max heap

--- algorithm/Test.js
+++ algorithm/Test.js
@@ -1,39 +1,30 @@
-function Merge(arr, begin, mid, end) {
-    let size1 = mid - begin + 1;
-    let size2 = end - mid;
+/**
+ * @param {Array} arr
+ * @param {Number} node
+ */
+function MaxHeapify(arr, node) {
+    let left = node * 2;
+    let right = node * 2 + 1;
+    let max = node;
 
-    let sub1 = arr.slice(begin, begin + size1);
-    let sub2 = arr.slice(mid + 1, mid + size2 + 1);
+    if (arr[left] < arr.length && arr[left] > arr[max]) max = left;
+    if (arr[right] < arr.length && arr[right] > arr[max]) max = right;
 
-    sub1.push(Infinity);
-    sub2.push(Infinity);
-
-    let i = 0,
-        j = 0;
-    for (let index = begin; index <= end; index++) {
-        if (sub1[i] <= sub2[j]) {
-            arr[index] = sub1[i];
-            i++;
-        } else {
-            arr[index] = sub2[j];
-            j++;
-        }
+    if (node !== max) {
+        [arr[node], arr[max]] = [arr[max], arr[node]];
+        MaxHeapify(arr, max);
     }
 }
 
-function MergeSort(arr, begin, end) {
-    if (begin < end) {
-        let mid = Math.floor((begin + end) / 2);
-        MergeSort(arr, begin, mid);
-        MergeSort(arr, mid + 1, end);
-        Merge(arr, begin, mid, end);
+/**
+ * @param {Array} arr
+ */
+function BuildMaxHeap(arr) {
+    for (let node = Math.floor(arr.length); node >= 1; node--) {
+        MaxHeapify(arr, node);
     }
 }
 
-// let arr = [1, 3, 5, 7, 9, 2, 6, 8, 10];
-// Merge(arr, 0, Math.floor(arr.length / 2), arr.length - 1);
-// console.log(arr);
-let arr = [1, 6, 4, 2, 5, 7, 9, 10, 16];
-MergeSort(arr, 0, arr.length - 1);
-
+let arr = [undefined, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+BuildMaxHeap(arr);
 console.log(arr);
