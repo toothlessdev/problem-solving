e18d98b5464e4d1cb4cc60df59f65937373b3182
toothlessdev <toothless042@gmail.com>
2024-06-27 20:30:35 +0900

    legacy

--- sort/SelectionSort.js
+++
@@ -1,9 +0,0 @@
-function SelectionSort(arr, size) {
-    for (let i = 0; i < size - 1; i++) {
-        let idx = i;
-        for (let j = i + 1; j < size; j++) {
-            if (arr[idx] > arr[j]) idx = j;
-        }
-        [arr[i], arr[idx]] = [arr[idx], arr[i]];
-    }
-}
