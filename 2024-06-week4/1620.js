e372dd5b16f4c139058dfc49c7df96513c9e2492
toothlessdev <toothless042@gmail.com>
2024-06-29 22:45:42 +0900

    1620

--- 2024-06-week4/1620.js
+++ 2024-06-week4/1620.js
@@ -5,4 +5,21 @@ let [meta, ...input] = fs.readFileSync(io).toString().trim().split("\n");
 
 let [n, m] = meta.split(" ");
 
-input.slice(0, n);
+let arr = input.slice(0, n);
+let test = input.slice(n);
+
+let nameDict = {};
+let indexDict = {};
+
+arr.forEach((name, index) => {
+    nameDict[name] = index;
+    indexDict[index] = name;
+});
+
+test.forEach((t) => {
+    if (Number(t)) {
+        console.log(indexDict[Number(t) - 1]);
+    } else {
+        console.log(nameDict[t] + 1);
+    }
+});
