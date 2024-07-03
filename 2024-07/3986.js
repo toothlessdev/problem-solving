22a29c488872fbc62738ed8b30639724f806c76b
toothlessdev <toothless042@gmail.com>
2024-07-03 22:50:33 +0900

    3986

--- 2024-07/3986.js
+++ 2024-07/3986.js
@@ -1,43 +1,21 @@
-/**
- * AAABBAAA 있을때,
- * 연속되는 문자열 그룹으로 나눈다
- * AAA | BB | AAA
- * 홀 | 짝 | 홀
- * 1) 짝수그룹은 짝수그룹 내부에서 충돌없이 연결가능
- * 2) 홀수그룹은 인접한 홀수그룹끼리 이어야 함 (충돌발생가능)
- * -> 홀수그룹 만 있는 경우 (충돌, 맞는짝없음)
- * -> 홀수그룹이 짝수개 (o)
- * -> 홀수그룹이 홀수개 (x)
- */
-
 const fs = require("fs");
 const io = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 
-let [n, ...tests] = fs.readFileSync(io).toString().split("\n");
+let [n, ...tests] = fs.readFileSync(io).toString().trim().split("\n");
 
 n = Number(n);
 
-let t = tests[0];
-
+let count = 0;
 for (let test of tests) {
-    let groupIndex = 1;
-    let oddGroupCount = 0;
-    let evenGroupCount = 0;
-
-    test = test.split("");
-
-    for (let index = 1; index < test.length; index++) {
-        let count = 0;
-        if (test[index - 1] !== test[index]) {
-            groupIndex++;
-            if (count % 2 === 0) evenGroupCount++;
-            else oddGroupCount++;
-        } else {
-            count++;
-        }
+    const stack = [];
+    for (let char of test.trim()) {
+        if (stack.length === 0) stack.push(char);
+        else if (stack.at(-1) === char) stack.pop();
+        else stack.push(char);
     }
-    console.log({ groupIndex, evenGroupCount, oddGroupCount });
+    if (stack.length === 0) count++;
 }
+console.log(count);
 
 /**
  * 오답노트
