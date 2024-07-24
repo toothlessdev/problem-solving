417cb64b2b3d6e96ce96f5598f7f5248618a4ad8
toothlessdev <toothless042@gmail.com>
2024-07-24 21:45:12 +0900

    4375

---
+++ 2024-07/4375.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const io = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+
+const [...inputs] = fs.readFileSync(io).toString().trim().split("\n").map(BigInt);
+
+for (const input of inputs) {
+    let number = "";
+    let digit = 1;
+    while (true) {
+        number = BigInt(number + "1") % BigInt(input);
+        digit++;
+        if (number % input === 0n) {
+            console.log(digit - 1);
+            break;
+        }
+    }
+}
+
+// MOD 연산의 성질
