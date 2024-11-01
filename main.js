8b8a7be31008a3a4c6c693350c5223804f5a9f04
toothlessdev <toothless042@gmail.com>
2024-11-01 23:30:00 +0900

    1992

--- main.js
+++ main.js
@@ -1,46 +1,36 @@
 const io = process.platform === "linux" ? "/dev/stdin" : "in.txt";
 const fs = require("fs");
 
-let [size, ...rows] = fs.readFileSync(io).toString().trim().split("\n");
-let blue = 0;
-let white = 0;
+let [size, ...arr] = fs.readFileSync(io).toString().trim().split("\n");
 
 size = +size;
+arr = arr.map((row) => row.split("").map(Number));
 
-const arr = rows.map((row) => row.split(" ").map(Number));
+console.log(divide(0, size, 0, size));
 
-function count(row_begin, row_end, col_begin, col_end) {
-    let flag = arr[row_begin][col_begin];
-
+function fill(row_begin, row_end, col_begin, col_end) {
+    const flag = arr[row_begin][col_begin];
     for (let row = row_begin; row < row_end; row++) {
         for (let col = col_begin; col < col_end; col++) {
-            if (flag !== arr[row][col]) {
-                return -1;
-            }
+            if (flag !== arr[row][col]) return -1;
         }
     }
     return flag;
 }
 
 function divide(row_begin, row_end, col_begin, col_end) {
-    let flag = count(row_begin, row_end, col_begin, col_end);
+    let filled = fill(row_begin, row_end, col_begin, col_end);
 
-    if (flag === -1) {
+    if (filled === -1) {
         let row_mid = parseInt((row_begin + row_end) / 2);
         let col_mid = parseInt((col_begin + col_end) / 2);
 
-        divide(row_begin, row_mid, col_begin, col_mid);
-
-        divide(row_mid, row_end, col_begin, col_mid);
-
-        divide(row_begin, row_mid, col_mid, col_end);
-
-        divide(row_mid, row_end, col_mid, col_end);
+        let qt1 = divide(row_begin, row_mid, col_begin, col_mid);
+        let qt2 = divide(row_begin, row_mid, col_mid, col_end);
+        let qt3 = divide(row_mid, row_end, col_begin, col_mid);
+        let qt4 = divide(row_mid, row_end, col_mid, col_end);
+        return `(${qt1}${qt2}${qt3}${qt4})`;
     } else {
-        if (flag === 0) white += 1;
-        else blue += 1;
+        return filled;
     }
 }
-
-divide(0, size, 0, size);
-console.log(white, blue);
